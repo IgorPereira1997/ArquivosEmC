e68e15556593d2ec5ef5f564ec2bd4ddf2084720
Igor Pereira <pereira.t.igor@gmail.com>
2020-09-09 20:38:57 -0300

    Added testBGI archive.

---
+++ testeBGI/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=Cygwin-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin-Windows
+CND_ARTIFACT_NAME_Debug=testebgi
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/testebgi
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin-Windows/package
+CND_PACKAGE_NAME_Debug=testebgi.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/testebgi.tar
+# Release configuration
+CND_PLATFORM_Release=Cygwin-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/Cygwin-Windows
+CND_ARTIFACT_NAME_Release=testebgi
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin-Windows/testebgi
+CND_PACKAGE_DIR_Release=dist/Release/Cygwin-Windows/package
+CND_PACKAGE_NAME_Release=testebgi.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin-Windows/package/testebgi.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
